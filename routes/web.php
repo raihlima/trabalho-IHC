85d693b66b81a70e78d258846d077871c9fe9b01
leoferenzini <leoferenzini@gmail.com>
2021-08-13 23:21:12 -0300

    Criação de rotas

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('professor',[App\Http\Controllers\ProfessorController::class,'index' ])->name('professor');
-Route::get('chefe',[App\Http\Controllers\EstudanteController::class,'index' ])->name('chefe');
+
+Route::get('chefe_departamento/professor',[App\Http\Controllers\ChefeDepartamentoController::class,'professor' ])->name('chefe_departamento.professor');
+Route::get('chefe_departamento/lista_professores',[App\Http\Controllers\ChefeDepartamentoController::class,'listaProfessores' ])->name('chefe_departamento.lista_professores');
+Route::get('chefe_departamento/lista_disciplinas',[App\Http\Controllers\ChefeDepartamentoController::class,'listaDisciplinas' ])->name('chefe_departamento.lista_disciplinas');
+
 Route::get('estudante',[App\Http\Controllers\EstudanteController::class,'index' ])->name('estudante');
 Route::get('resultado_avaliacao',[App\Http\Controllers\ResultadoAvaliacaoController::class,'index' ])->name('resultado_avaliacao');

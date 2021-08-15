c9f71c1e5655134f9349fd48a98013a92d590706
raihlima <raih.cesar@gmail.com>
2021-08-15 16:12:26 -0300

    Corrigido erro de login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Matrícula ou senha inválida(s).',
+    'password' => 'Matrícula ou senha inválida(s).',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Matricula') }}</label>
+                            <label for="matricula" class="col-md-4 col-form-label text-md-right">{{ __('Matricula') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="matricula" value="{{ old('matricula') }}" required autocomplete="matricula" autofocus>
+                                <input id="matricula" type="text" class="form-control @error('matricula') is-invalid @enderror" name="matricula" value="{{ old('matricula') }}" required autocomplete="matricula" autofocus>
 
                                 @error('matricula')
                                     <span class="invalid-feedback" role="alert">
